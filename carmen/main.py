9f24ed55048d8e0a6928a2597eca446af386c441
Tundish <tundish@thuswise.org>
2018-03-20 20:52:05 +0000

    Doc fix.

--- carmen/main.py
+++ carmen/main.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # encoding: UTF-8
 
-# This file is part of Addison Arches.
+# This file is part of Carmen Brittunculi.
 #
-# Addison Arches is free software: you can redistribute it and/or modify it
+# Carmen Brittunculi is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Addison Arches is distributed in the hope that it will be useful, but
+# Carmen Brittunculi is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Addison Arches.  If not, see <http://www.gnu.org/licenses/>.
+# along with Carmen Brittunculi.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 from collections import namedtuple
