fa17cd538f40c4b4a3a47f3bb146e97ce7508c08
Tundish <tundish@thuswise.org>
2018-04-02 12:58:38 +0100

    Starting and stopping registry.

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 PROJ=carmen
 
+REG=`sudo systemd-run --slice=machine rkt run --insecure-options=image docker://registry`
+
 mkdir -p dist
-#sudo rkt run --insecure-options=image docker://registry
 
 docker rmi ${PROJ}_app
 docker build -t ${PROJ}_app:latest .
@@ -11,3 +12,5 @@ docker build -t ${PROJ}_app:latest .
 echo "Docker image: " `docker images -q ${PROJ}_app`
 
 rkt fetch --insecure-options=image docker://localhost:5000/${PROJ}_app:latest
+
+echo ${REG} | cut -d: -f2 | xargs sudo systemctl stop
