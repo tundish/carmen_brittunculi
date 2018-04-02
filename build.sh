45c25340e24df29db5c04c6f70a409e83b58fc7c
Tundish <tundish@thuswise.org>
2018-04-02 13:14:02 +0100

    Debug starting and stopping registry.

--- build.sh
+++ build.sh
@@ -15,5 +15,4 @@ echo "Docker image: " `docker images -q ${PROJ}_app`
 rkt fetch --insecure-options=image docker://localhost:5000/${PROJ}_app:latest
 
 echo "Stopping registry"
-#echo ${REG} | cut -d: -f2 | xargs sudo systemctl stop
-echo ${REG}
+echo ${REG} | cut -d: -f2 | xargs sudo systemctl stop
