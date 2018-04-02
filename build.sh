24ca103a919845216640384fced3dc2b96e0b112
Tundish <tundish@thuswise.org>
2018-04-02 16:47:52 +0100

    Debug starting and stopping registry.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #! /bin/sh -
 
 PROJ=carmen
+HOST=`hostname`
 
 echo "Starting temporary registry"
 docker run -d -p 5000:5000 registry:2
@@ -8,12 +9,12 @@ docker run -d -p 5000:5000 registry:2
 mkdir -p dist
 
 docker rmi ${PROJ}_app
-docker build -t localhost:5000/${PROJ}_app:latest .
+docker build -t ${HOST}:5000/${PROJ}_app:latest .
 
 echo "Docker image: " `docker images -q ${PROJ}_app`
-docker push localhost:5000/${PROJ}_app:latest
+docker push ${HOST}:5000/${PROJ}_app:latest
 
-rkt fetch --insecure-options=image docker://localhost:5000/${PROJ}_app:latest
+rkt fetch --insecure-options=image docker://${HOST}:5000/${PROJ}_app:latest
 
 echo "Stopping registry"
 docker stop `docker ps | grep registry | cut -d' ' -f1`
