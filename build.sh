1e072e5e2faaa75180ad563d4951ce01ef768d73
Tundish <tundish@thuswise.org>
2018-04-02 10:56:23 +0100

    First rkt fetch attempt.

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@
 PROJ=carmen
 
 mkdir -p dist
+
 docker rmi ${PROJ}_app
 docker build -t ${PROJ}_app:latest .
+
+echo "Docker image: " `docker images -q ${PROJ}_app`
+
+rkt fetch docker://localhost:5000/${PROJ}_app:latest
