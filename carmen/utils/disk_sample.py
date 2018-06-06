20b669a9f49b93e4d02f523eb0209b5b7b4288ae
Tundish <tundish@gigeconomy.org.uk>
2018-06-06 14:11:51 +0100

    First SVG output.

--- carmen/utils/disk_sample.py
+++ carmen/utils/disk_sample.py
@@ -16,4 +16,34 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Carmen Brittunculi.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
 
+svg = """
+<svg xmlns="http://www.w3.org/2000/svg"
+xmlns:xlink="http://www.w3.org/1999/xlink"
+width="{width}" height="{height}"
+viewBox="0 0 {width} {height}"
+preserveAspectRatio="none"
+>
+{0}
+</svg>
+"""
+
+spot = """
+<circle cx="{0.real:.0}" cy="{0.imag:.0}" r="2"
+stroke="gray" stroke-width="4"
+fill="dimgrey"
+/>
+"""
+
+WIDTH = 600
+HEIGHT = 400
+
+points = [complex(0, 0), complex(WIDTH, HEIGHT)]
+
+def paint(points, width=WIDTH, height=HEIGHT):
+    content = "\n".join(spot.format(point) for point in points)
+    return svg.format(content, width=WIDTH, height=HEIGHT)
+
+if __name__ == "__main__":
+    sys.stdout.write(paint(points))
