b4d3f555d7a74e8eb2772aa905649ebaefcdbf71
Tundish <tundish@thuswise.org>
2018-06-27 18:02:07 +0100

    Start to populate Motivator.

--- carmen/motivator.py
+++ carmen/motivator.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Carmen Brittunculi.  If not, see <http://www.gnu.org/licenses/>.
 
+from collections import namedtuple
 from enum import Enum
 
 class Clock(Enum):
@@ -25,5 +26,21 @@ class Clock(Enum):
 
 class Motivator:
 
+    Move = namedtuple("Move", ["entity", "vector", "hop"])
+
     def __init__(self, actor):
         self.actor = actor
+
+    def transport(self, finder, destination=None):
+        here = self.actor.get_state(Spot)
+        location = next(
+            i for i in finder.ensemble()
+            if isinstance(i, Location) and i.get_state(Spot) == here
+        )
+        route = finder.route(location, destination, maxlen=20)
+        for hop in route:
+            spot = hop.get_state(Spot)
+            vector = spot.value - here.value
+            yield Move(self.actor, vector, hop)
+            here = spot
+
