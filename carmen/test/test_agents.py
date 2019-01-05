a66b329c6927e730a6896354528637954f4533ad
Tundish <tundish@thuswise.org>
2019-01-05 13:40:49 +0000

    Add unit test module prior to agent refactor.

---
+++ carmen/test/test_agents.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+# This file is part of Carmen Brittunculi.
+#
+# Carmen Brittunculi is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Carmen Brittunculi is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Carmen Brittunculi.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from carmen.agents import Clock
+
+class ClockTests(unittest.TestCase):
+
+    def test_tick(self):
+        clock = Clock()
+        self.fail(clock)
