21d8429abe74caa0fee6f3fb5e66c36862b9aad5
Tundish <tundish@gigeconomy.org.uk>
2019-03-05 18:57:22 +0000

    Scenery transform styling not working yet.

--- carmen/utils/scenery.py
+++ carmen/utils/scenery.py
@@ -29,8 +29,6 @@ from collections import deque
 import random
 import sys
 
-DEFAULT_SYMBOL = "spot"
-
 svg = """
 <svg xmlns="http://www.w3.org/2000/svg"
 xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -44,48 +42,22 @@ preserveAspectRatio="none"
 
 use = """
 <use class="{klass}"
-x="{point.real:.0f}" y="{point.imag:.0f}"
+x="{point.real:.0f}" y="{point.imag:.0f}" transform="inherit"
 xlink:href="#{name}" />
 """
 
-symbols = {
-    5: [
-        ("spot", """
-<symbol id="spot">
-<circle stroke="red" fill="dimgrey" stroke-width="1"
-cx="3" cy="3" r="2"
-/>
-</symbol>
-                """),
-    ],
-    24: [
-        ("svg-leaf-00", """
-<symbol id="svg-leaf-00">
-<path
-    d="M 12 11 C 21,2 17,22 23,27 C 8,25 3,11 12,11 z M 18,16 C 16,14 17,22 20,24 z"
-    stroke-width="0.2"
-    stroke="yellow"
-    fill="currentColor"
-    fill-rule="evenodd"
-/>
-</symbol>
-                """),
-    ],
-    32: [
-        ("svg-leaf-01", """
-<symbol id="svg-leaf-01">
-<path
-    d="M 12 11 C 21,2 17,22 23,27 C 8,25 3,11 12,11 z M 18,16 C 16,14 17,22 20,24 z"
-    stroke-width="0.2"
-    stroke="yellow"
-    fill="currentColor"
-    fill-rule="evenodd"
-    transform="scale(1.4) rotate(45 16 16)"
-/>
-</symbol>
-                """),
-    ],
+style = """
+<style>
+/* <![CDATA[ */
+use.med {
+    transform="scale(1.4)";
 }
+use.r45 {
+    transform="rotate(45 16 16)";
+}
+/* ]]> */
+</style>
+"""
 
 symbols = {
 "spot": (5, """
@@ -94,14 +66,15 @@ symbols = {
 cx="3" cy="3" r="2"
 />
 </symbol>"""),
-"leaf": (24, """
-<symbol id="svg-leaf">
+"leaf": (16, """
+<symbol id="leaf">
 <path
     d="M 12 11 C 21,2 17,22 23,27 C 8,25 3,11 12,11 z M 18,16 C 16,14 17,22 20,24 z"
     stroke-width="0.2"
     stroke="yellow"
-    fill="currentColor"
+    fill="green"
     fill-rule="evenodd"
+    transform="inherit"
 />
 </symbol>"""),
 }
@@ -109,6 +82,8 @@ cx="3" cy="3" r="2"
 DEFAULT_WIDTH = 360
 DEFAULT_HEIGHT = 600
 DEFAULT_POINTS = 600
+DEFAULT_SYMBOL = "spot"
+
 
 def overlaps(this, that, span):
     return cmath.isclose(this, that, abs_tol=span)
@@ -143,9 +118,10 @@ def poisson_disk(
                 yield p, gap
 
 def paint(points, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT):
-    content = [s for n, s in symbols.values()]
+    content = [style]
+    content.extend([s.strip() for n, s in symbols.values()])
     content.extend([
-        use.format(klass=klass, name=name, point=point)
+        use.format(klass=klass, name=name, point=point).strip()
         for klass, name, point in points
     ])
     return svg.format("\n".join(content), width=width, height=height)
@@ -160,7 +136,7 @@ def parser(description=__doc__):
         help="Give a sequence of pixel sizes for spacing."
     )
     rv.add_argument(
-        "--symbol", action="append", type=str, default=[DEFAULT_SYMBOL],
+        "--symbol", action="append", type=str, default=[],
         help="Pick one or more symbol to use ({0}) [{1}].".format(
             ",".join(symbols.keys()), DEFAULT_SYMBOL
         )
@@ -185,13 +161,14 @@ def parser(description=__doc__):
 
 def main(args):
     scene = []
-    spacing = [symbols[name][0] for name in args.symbol]
+    names = args.symbol or [DEFAULT_SYMBOL]
+    spacing = [symbols[name][0] for name in names]
     lookup = defaultdict(list)
-    for name in args.symbol:
+    for name in names:
         gap, data = symbols[name]
         lookup[gap].append(name)
 
-    classes = ["plain"]
+    classes = ["plain", "r45", "med"]
     for n, (point, gap) in enumerate(
         poisson_disk(
             args.points,
@@ -207,7 +184,7 @@ def main(args):
         name = random.choice(lookup[gap])
         scene.append((random.choice(classes), name, point))
         if args.debug:
-            print(point, file=sys.stderr)
+            print(name, gap, point, file=sys.stderr)
 
     print("{0} items".format(n), file=sys.stderr)
     print(paint(scene, args.width, args.height), file=sys.stdout)
